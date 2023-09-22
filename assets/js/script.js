b73927d319a0b354fcce8d44890184e35da1fb78
GlennJo <139962883+GlennJohansson85@users.noreply.github.com>
2023-09-22 21:50:58 +0200

    js
    Add 2 codes in "function runGame"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,58 @@
-function runGame(){}
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-function checkAnswer(){}
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
-function calculateCorrectAnswer(){}
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
 
-function incrementScore(){}
+});
 
-function incrementWrongAnswer(){}
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
 
-function displayAdditionQuestion(){}
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-function displaySubtractQuestion(){}
+}
 
-function displayMulityplyQuestion(){}
+function checkAnswer() {
 
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
