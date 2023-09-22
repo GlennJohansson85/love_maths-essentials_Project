3962b25752d300f8f56e3b5076a94a2742c51695
GlennJo <139962883+GlennJohansson85@users.noreply.github.com>
2023-09-22 20:32:50 +0200

    js.script
    Added  8 functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
-console.log(connected!);
-            
+function runGame(){}
+
+function checkAnswer(){}
+
+function calculateCorrectAnswer(){}
+
+function incrementScore(){}
+
+function incrementWrongAnswer(){}
+
+function displayAdditionQuestion(){}
+
+function displaySubtractQuestion(){}
+
+function displayMulityplyQuestion(){}
+
